502eb826a4322054b73b7f5af98d9a1a5a79675d
Eric Kidd <git@randomhacks.net>
2011-09-10 21:57:27 -0400

    Add rename-file-and-buffer from Steve Yegge's .emacs

--- emk-commands.el
+++ emk-commands.el
@@ -61,3 +61,38 @@ when the function is called via `format-alist'."
   (interactive)
   (let ((path (format-time-string "~/doc/todo/%Y%m%d.txt")))
     (find-file path)))
+
+;; From https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+    (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+	(progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+;; From https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+(defun move-file-and-buffer (dir)
+ "Moves both current buffer and file it's visiting to DIR."
+ (interactive "DNew directory: ")
+ (let* ((name (buffer-name))
+        (filename (buffer-file-name))
+        (dir
+	 (if (string-match dir "\\(?:/\\|\\\\)$")
+             (substring dir 0 -1) dir))
+        (newname (concat dir "/" name)))
+   (if (not filename)
+       (message "Buffer '%s' is not visiting a file!" name)
+     (progn
+       (copy-file filename newname 1)
+       (delete-file filename)
+       (set-visited-file-name newname)
+       (set-buffer-modified-p nil)
+       t))))
